19582141cab4d54744f71712c1474d8d7f0a9fb2
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-26 20:23:35 +0200

    Add files via upload

---
+++ C#-Programming-Basics/Homeworks/Conditional Statements/03.SpeedInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.SpeedInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
