c2a2d379ec7e2f8924c85cc89d22989455fdad3b
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-26 20:49:56 +0200

    Create Program.cs

---
+++ C# Programming Basics/Homeworks/Conditional Statements Advanced/02.SummerOutfit/Program.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Xml;
+
+namespace _02.SummerOutfit
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+
+            int degrees = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+
+            if (timeOfDay == "Morning")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Sweatshirt and Sneakers.");
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+                }
+                else if (degrees >= 25)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your T-Shirt and Sandals.");
+                }
+            }
+
+            else if (timeOfDay == "Afternoon")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your T-Shirt and Sandals.");
+
+                }
+                else if (degrees >= 25)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Swim Suit and Barefoot.");
+
+                }
+            }
+
+            else if (timeOfDay == "Evening")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+
+                }
+                else if (degrees >= 25)
+                {
+                    Console.WriteLine($"It's {degrees} degrees, get your Shirt and Moccasins.");
+
+                }              
+            }
+        }
+    }
+}
