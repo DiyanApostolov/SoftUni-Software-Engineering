0ca0f701771833e9e9171929d3ce55ea2dbbaf1c
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-26 20:12:24 +0200

    Add files via upload

---
+++ C# Programming Basics/Homeworks/Conditional Statements/01. Sum Seconds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalTime = 0;
+            totalTime += int.Parse(Console.ReadLine());
+            totalTime += int.Parse(Console.ReadLine());
+            totalTime += int.Parse(Console.ReadLine());
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:d2}");
+        }
+    }
+}
