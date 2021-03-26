8e3d5d8d2427d2b2e6c69dde6f3c3290d71e6b21
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-26 20:21:09 +0200

    Add files via upload

---
+++ C# Programming Basics/Homeworks/Conditional Statements/02.BonusScore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            double bonus = 0.0;
+
+            if (number < 100)
+            {
+                bonus = 5;
+            }
+            else if (number > 1000)
+            {
+                bonus = number * 0.1;
+            }
+            else
+            {
+                bonus = number * 0.2;
+            }
+
+            if (number % 2 == 0)
+            {
+                bonus = bonus + 1;
+            }
+            else if (number % 10 == 5)
+            {
+                bonus += 2;
+            }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(number + bonus);
+        }
+    }
+}
