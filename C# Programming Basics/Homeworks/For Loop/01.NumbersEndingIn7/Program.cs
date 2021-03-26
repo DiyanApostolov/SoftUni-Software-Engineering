c64309ffc54e3b812bb34e7ebb70166cab80d3f8
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-26 21:06:33 +0200

    Create Program.cs

---
+++ C# Programming Basics/Homeworks/For Loop/01.NumbersEndingIn7/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _01.NumbersEndingIn7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i+=10)
+            {
+                Console.WriteLine(i);
+            }    
+        }
+    }
+}
