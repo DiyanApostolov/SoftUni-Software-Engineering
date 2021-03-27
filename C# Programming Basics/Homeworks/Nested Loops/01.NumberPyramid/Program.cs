55814763fa3dadba730bca554448c06bc95c211b
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-03-27 22:59:15 +0200

    Create Program.cs

---
+++ C# Programming Basics/Homeworks/Nested Loops/01.NumberPyramid/Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace _01.NumberPyramid
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int current = 1;
+            bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int colums = 1; colums <= rows; colums++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                
+                if (isBigger)
+                {
+                    break;
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
