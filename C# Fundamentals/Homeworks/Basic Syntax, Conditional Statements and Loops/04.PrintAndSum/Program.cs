a788438f71d3eac7d9b158a4b3709820ecb20f96
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-07 16:48:03 +0300

    Create Program.cs

---
+++ C# Fundamentals/Homeworks/Basic Syntax, Conditional Statements and Loops/04.PrintAndSum/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace _04.PrintAndSum
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
