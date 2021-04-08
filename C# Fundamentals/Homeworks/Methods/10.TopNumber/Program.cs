654499ed9c1b3a73b23f96248a0a8b7aea4fffba
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-08 16:22:48 +0300

    Create Program.cs

---
+++ C# Fundamentals/Homeworks/Methods/10.TopNumber/Program.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace _10.TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintingTopNumbers(number);
+        }
+
+        private static void PrintingTopNumbers(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                int sum = 0;
+
+                int currentNumber = i;
+                int numberToDivide = currentNumber;
+
+                int counterOfOddDigits = 0;
+
+                while (numberToDivide != 0)
+                {
+                    currentNumber = numberToDivide % 10;
+                    numberToDivide = numberToDivide / 10;
+
+                    if (currentNumber % 2 != 0)
+                    {
+                        counterOfOddDigits++;
+                    }
+
+                    sum = sum + currentNumber;
+                }
+
+                if (sum % 8 == 0 && counterOfOddDigits >= 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+© 2021 GitHub, Inc.
