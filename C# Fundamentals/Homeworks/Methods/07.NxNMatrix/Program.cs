46413bc3f6fdf073496a7c3bd933be29270ec202
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-08 16:15:33 +0300

    Create Program.cs

---
+++ C# Fundamentals/Homeworks/Methods/07.NxNMatrix/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace _07.NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintMatrix(n);
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
