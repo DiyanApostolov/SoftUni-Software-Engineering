67585b23a16a8cffddaa91faf0e7e1df7212b72d
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-21 00:03:35 +0300

    Add homework

--- C# Advanced/Homeworks-And-Labs/02.MultidimensionalArrays-Exercise/06.JaggedArrayManipulator/Program.cs
+++ C# Advanced/Homeworks-And-Labs/02.MultidimensionalArrays-Exercise/06.JaggedArrayManipulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06.JaggedArrayManipulator
 {
@@ -6,7 +7,79 @@ namespace _06.JaggedArrayManipulator
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            double[][] matrix = new double [n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                double[] rowdata = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+                matrix[row] = rowdata;
+            }
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row + 1].Length)
+                {
+                    for (int i = row; i < row + 2; i++)
+                    {
+                        for (int j = 0; j < matrix[row].Length; j++)
+                        {
+                            matrix[i][j] *= 2;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = row; i < row + 2; i++)
+                    {
+                        for (int j = 0; j < matrix[i].Length; j++)
+                        {
+                            matrix[i][j] /= 2;
+                        }
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] cmdArg = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int row = int.Parse(cmdArg[1]);
+                int col = int.Parse(cmdArg[2]);
+                int value = int.Parse(cmdArg[3]);
+
+                if (cmdArg[0] == "Add")
+                {
+                    if (row >= 0 && row < n && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] += value;
+                    }
+                }
+                else if (cmdArg[0] == "Subtract")
+                {
+                    if (row >= 0 && row < n && col >= 0 && col < matrix[row].Length)
+                    {
+                        matrix[row][col] -= value;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ C# Advanced/Homeworks-And-Labs/02.MultidimensionalArrays-Exercise/07.KnightGame/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace _07.KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int removedKnights = 0;
+
+            while (true)
+            {
+                int knightRow = int.MinValue;
+                int knightCol = int.MinValue;
+                int totalAttacks = 0;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int tempAttacks = CountAttack(matrix, row, col);
+
+                            if (tempAttacks > totalAttacks)
+                            {
+                                totalAttacks = tempAttacks;
+                                knightRow = row;
+                                knightCol = col;
+                            }
+                        }
+                    }
+                }
+
+                if (totalAttacks > 0)
+                {
+                    matrix[knightRow, knightCol] = '0';
+                    removedKnights++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(removedKnights);
+        }
+
+        private static int CountAttack(char[,] matrix, int row, int col)
+        {
+            int attacks = 0;
+
+            if (IsValid(row - 2, col + 1, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row - 2, col - 1, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row - 1, col - 2, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row + 1, col - 2, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row + 2, col - 1, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row + 2, col + 1, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row - 1, col + 2, matrix))
+            {
+                attacks++;
+            }
+            if (IsValid(row + 1, col + 2, matrix))
+            {
+                attacks++;
+            }
+
+            return attacks;
+        }
+
+        private static bool IsValid(int row, int col, char[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0)
+                && col >= 0 && col < matrix.GetLength(1)
+                && matrix[row, col] == 'K';
+        }
+    }
+}
