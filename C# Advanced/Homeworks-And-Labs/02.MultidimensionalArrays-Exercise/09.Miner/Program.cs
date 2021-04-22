c6c8563e043ae8cd2d3e5c024887a5c16d2cb3bf
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-22 20:53:47 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/02.MultidimensionalArrays-Exercise/09.Miner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
