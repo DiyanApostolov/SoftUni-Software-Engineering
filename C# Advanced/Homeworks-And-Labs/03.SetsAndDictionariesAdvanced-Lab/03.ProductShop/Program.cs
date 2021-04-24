b8d3f942a75f349c1cda9c088209823cc97326ae
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-24 23:05:00 +0300

    Add Sets and Dictionaries homework

---
+++ C# Advanced/Homeworks-And-Labs/03.SetsAndDictionariesAdvanced-Lab/03.ProductShop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> stores = new SortedDictionary<string, Dictionary<string, double>>();
+
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "Revision")
+            {
+                string store = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!stores.ContainsKey(store))
+                {
+                    stores.Add(store, new Dictionary<string, double>());
+                }
+
+                stores[store].Add(product, price);
+
+                input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var store in stores)
+            {
+                Console.WriteLine($"{store.Key}->");
+
+                foreach (var product in store.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
