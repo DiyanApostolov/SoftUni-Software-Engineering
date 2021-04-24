ffe168753ebc802a7082ca24d626f5e899595382
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-24 22:33:06 +0300

    Add Sets and Dictionary lab

---
+++ C# Advanced/Homeworks-And-Labs/03.SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> times = new Dictionary<double, int>();
+
+            foreach (var item in input)
+            {
+                if (!times.ContainsKey(item))
+                {
+                    times.Add(item, 0);
+                }
+
+                times[item]++;
+            }
+
+            foreach (var item in times)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
