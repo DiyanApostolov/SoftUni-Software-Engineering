690cc54474dbff2af95e6b5b3feede20d8262eee
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-04-26 19:55:50 +0300

    Update Program.cs

--- C# Advanced/Homeworks-And-Labs/03.SetsAndDictionariesAdvanced-Lab/04.CitiesByContinentAndCountry/Program.cs
+++ C# Advanced/Homeworks-And-Labs/03.SetsAndDictionariesAdvanced-Lab/04.CitiesByContinentAndCountry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04.CitiesByContinentAndCountry
 {
@@ -6,7 +7,40 @@ namespace _04.CitiesByContinentAndCountry
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);
+            }
+            Console.WriteLine();
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($"   {country.Key} -> {string.Join(", ", country.Value)}");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
