97785b4567fe1ccb0ca0f3477499c686e449a522
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-13 15:00:01 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Lab/4.MergeFiles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _4.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<string>();
+
+            using (StreamReader firstInput = new StreamReader("../../../Input1.txt"))
+            {
+                using (StreamReader secondInput = new StreamReader("../../../Input2.txt"))
+                {
+                    string firstRow = firstInput.ReadLine();
+                    string secondRow = secondInput.ReadLine();
+
+                    while (firstRow != null || secondRow != null)
+                    {
+                        lines.Add(firstRow);
+
+                        if (secondRow != null)
+                        {
+                            lines.Add(secondRow);
+                        }
+
+                        firstRow = firstInput.ReadLine();
+                        secondRow = secondInput.ReadLine();
+                    }
+                }
+            }
+
+            using (StreamWriter output = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (var line in lines)
+                {
+                    output.WriteLine(line);
+                }
+            }
+        }
+    }
+}
