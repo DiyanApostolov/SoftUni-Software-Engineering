63e1432e6a653e6731bb86714633058110da4c73
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-13 07:51:04 +0300

    Add homework

--- C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Lab/1.OddLines/Program.cs
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Lab/1.OddLines/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _1.OddLines
 {
@@ -6,7 +7,22 @@ namespace _1.OddLines
     {
         static void Main(string[] args)
         {
-            
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                while (currentRow != null)
+                {
+                    if (row % 2 == 1)
+                    {
+                        Console.WriteLine(currentRow);
+                    }
+
+                    currentRow = reader.ReadLine();
+                    row++;
+                }
+            }
         }
     }
 }
