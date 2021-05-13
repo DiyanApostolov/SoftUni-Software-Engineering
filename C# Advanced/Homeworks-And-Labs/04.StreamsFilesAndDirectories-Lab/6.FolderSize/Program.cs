adac68d081e6efea5402c7825ee57e1a2ea2c9b6
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-13 20:19:02 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Lab/6.FolderSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _6.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../TestFolder");
+            double sum = 0;
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+                sum += file.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("../../../Output.txt", sum.ToString());
+        }
+    }
+}
