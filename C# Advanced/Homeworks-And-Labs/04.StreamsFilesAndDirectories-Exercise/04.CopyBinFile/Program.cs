a879673719d20c7100bada5284ad6d914ec02207
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-17 20:15:54 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Exercise/04.CopyBinFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream streamReader = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (var streamWriter = new FileStream("../../../picCopy.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        var buffer = new byte[4096];
+
+                        int readSize = streamReader.Read(buffer, 0, buffer.Length);
+
+                        if (readSize == 0)
+                        {
+                            break;
+                        }
+
+                        streamWriter.Write(buffer, 0, readSize);
+                    }
+                }
+            }
+        }
+    }
+}
