6b493156994e799cb66876943a09635c3c9107eb
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-17 17:19:19 +0300

    Add homework

--- C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Exercise/01.EvenLines/Program.cs
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Exercise/01.EvenLines/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace _01.EvenLines
 {
@@ -6,7 +8,40 @@ namespace _01.EvenLines
     {
         static void Main(string[] args)
         {
-            
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                while (currentRow != null)
+                {
+                    if (row % 2 == 0)
+                    {
+                        string replacedRow = ReplaceSymbols(currentRow);
+
+                        var reversedRow = replacedRow
+                            .Split()
+                            .Reverse()
+                            .ToArray();
+
+                        Console.WriteLine(string.Join(' ', reversedRow));
+                    }
+
+                    row++;
+                    
+                    currentRow = reader.ReadLine();
+                }
+            }
+        }
+
+        private static string ReplaceSymbols(string currentRow)
+        {
+            return currentRow
+                        .Replace("-", "@")
+                        .Replace(",", "@")
+                        .Replace(".", "@")
+                        .Replace("!", "@")
+                        .Replace("?", "@");
         }
     }
 }
---
+++ C# Advanced/Homeworks-And-Labs/04.StreamsFilesAndDirectories-Exercise/03.WordCount/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textLines = File.ReadAllLines("../../../text.txt");
+            string[] words = File.ReadAllLines("../../../words.txt");
+
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string lowercaseWord = word.ToLower();
+
+                if (!wordsCount.ContainsKey(lowercaseWord))
+                {
+                    wordsCount.Add(lowercaseWord, 0);
+                }
+            }
+
+            foreach (var line in textLines)
+            {
+                string[] currentRow = line.ToLower().Split(new char[] { ' ', '-', ',', '!', '?', '.', '\'' });
+
+                foreach (var currentWord in currentRow)
+                {
+                    if (wordsCount.ContainsKey(currentWord))
+                    {
+                        wordsCount[currentWord]++;
+                    }
+                }
+            }
+
+            string actualResultPath = "../../../actualResult.txt";
+            string expectedResultPath = "../../../expectedResult.txt";
+
+            foreach (var kvp in wordsCount)
+            {
+                File.AppendAllText(actualResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+
+            foreach (var kvp in wordsCount.OrderByDescending(x => x.Value))
+            {
+                File.AppendAllText(expectedResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+        }
+    }
+}
