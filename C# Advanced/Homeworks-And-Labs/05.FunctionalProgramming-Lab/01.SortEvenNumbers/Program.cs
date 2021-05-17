bdcab5da0b0b906051550bd6a5425c811f8127c7
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-17 20:53:12 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> evenNumbers = new List<int>();
+
+            int[] input = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var number in input)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(',', evenNumbers.OrderBy(n => n)));
+        }
+    }
+}
