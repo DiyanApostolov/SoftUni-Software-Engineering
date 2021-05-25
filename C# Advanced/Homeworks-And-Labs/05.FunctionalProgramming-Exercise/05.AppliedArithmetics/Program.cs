57312b1b5fb20d0008f256db3e808fa7fdbb250e
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-26 00:53:08 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            Func<List<int>, List<int>> func = n =>
+            {
+                List<int> newNumbers = new List<int>();
+
+                if (command == "add")
+                {
+                    foreach (var item in n)
+                    {
+                        newNumbers.Add(item + 1);
+                    }
+                }
+                else if (command == "multiply")
+                {
+                    foreach (var item in n)
+                    {
+                        newNumbers.Add(item * 2);
+                    }
+                }
+                else if (command == "subtract")
+                {
+                    foreach (var item in n)
+                    {
+                        newNumbers.Add(item - 1);
+                    }
+                }
+
+                return newNumbers;
+            }; 
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                    case "multiply":
+                    case "subtract":
+                        numbers = func(numbers);
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(' ', numbers));
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
