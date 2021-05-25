5a727640235feacdd481d9f1878d3f41a3c7a3e9
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-26 00:25:19 +0300

    Update Program.cs

--- C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/04.FindEvensOrOdds/Program.cs
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/04.FindEvensOrOdds/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.FindEvensOrOdds
 {
@@ -6,7 +8,57 @@ namespace _04.FindEvensOrOdds
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] bounds = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = bounds[0];
+            int end = bounds[1];
+
+            Func<int, int, List<int>> generateRageOfNums = (s, e) =>
+            {
+                List<int> nums = new List<int>();
+
+                for (int i = s; i <= e; i++)
+                {
+                    nums.Add(i);
+                }
+
+                return nums;
+            };
+
+            List<int> numbers = generateRageOfNums(start, end);
+
+            Predicate<int> predicate = n => true;
+
+            string criteria = Console.ReadLine();
+
+            if (criteria == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+            else if (criteria == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+
+            Console.WriteLine(string.Join(' ', MyWhere(numbers, predicate)));
+        }
+
+        private static List<int> MyWhere(List<int> numbers, Predicate<int> predicate)
+        {
+            List<int> newNumbers = new List<int>();
+
+            foreach (var curNum in numbers)
+            {
+                if (predicate(curNum))
+                {
+                    newNumbers.Add(curNum);
+                }
+            }
+
+            return newNumbers;
         }
     }
 }
