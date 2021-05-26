2f7d26ae21f2397850c62262e1943d945c7d323b
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-26 09:21:53 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int[], int, List<int>> reverseAndExclude = (arr, n) => 
+            {
+                Array.Reverse(arr);
+                List<int> list = new List<int>();
+
+                foreach (var num in arr)
+                {
+                    if (num % n != 0)
+                    {
+                        list.Add(num);
+                    }
+                }
+
+                return list;
+            };
+
+            Console.WriteLine(string.Join(' ', reverseAndExclude(input, n)));
+        }
+    }
+}
---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLenght = int.Parse(Console.ReadLine());
+
+            Func<string, bool> filterFunc = x => x.Length <= nameLenght;
+
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(filterFunc)
+                .ToList();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
