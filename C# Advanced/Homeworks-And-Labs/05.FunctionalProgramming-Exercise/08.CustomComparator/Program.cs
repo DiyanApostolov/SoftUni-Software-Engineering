2bd1d44a48a214b24f2d3f9ea92614d06e08b577
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-05-26 17:19:46 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/08.CustomComparator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> sortFunc = (x, y) =>
+                (x % 2 == 0 && y % 2 != 0) ? -1
+                : (x % 2 != 0 && y % 2 == 0) ? 1
+                : x > y ? 1 : x < y ? -1 : 0;
+
+            Array.Sort(inputNumbers, (x, y) => sortFunc(x, y));
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+        }
+    }
+}
---
+++ C# Advanced/Homeworks-And-Labs/05.FunctionalProgramming-Exercise/09.ListOfPredicates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+        }
+    }
+}
