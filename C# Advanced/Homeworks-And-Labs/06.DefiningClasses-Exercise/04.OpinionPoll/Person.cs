129bb3a899411b86b99b261b088f9125c940ed5d
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-01 09:50:50 +0300

    Add homework

---
+++ C# Advanced/Homeworks-And-Labs/06.DefiningClasses-Exercise/04.OpinionPoll/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age)
+        {
+            Name = "No name";
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ C# Advanced/Homeworks-And-Labs/06.DefiningClasses-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string personName = personInfo[0];
+                int personAge = int.Parse(personInfo[1]);
+
+                Person currentPerson = new Person(personName, personAge);
+
+                persons.Add(currentPerson);
+            }
+
+            foreach (var person in persons
+                .OrderBy(p => p.Name)
+                .Where(p => p.Age > 30))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
