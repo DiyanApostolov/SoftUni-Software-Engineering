36c28e09782dd77a00fa4df6ca84aa4b8cbaa26c
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-06 22:59:40 +0300

    Add homework for Defining Classes

---
+++ C# Advanced/Homeworks-And-Labs/06.DefiningClasses-Exercise/08.CarSalesman/Car.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine, weight)
+        {
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"{this.Model}:");
+            result.AppendLine($"{this.Engine}");
+
+            if (this.Weight == 0)
+            {
+                result.AppendLine($"  Weight: n/a");
+            }
+            else
+            {
+                result.AppendLine($"  Weight: {this.Weight}");
+            }
+
+            if (this.Color == null)
+            {
+                result.Append($"  Color: n/a");
+            }
+            else
+            {
+                result.Append($"  Color: {this.Color}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
