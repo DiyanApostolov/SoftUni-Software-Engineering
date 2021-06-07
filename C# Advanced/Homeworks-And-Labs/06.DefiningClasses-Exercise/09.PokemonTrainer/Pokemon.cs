1cfbdde83212d2f688df4ba5f94054cd260dcd89
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-07 23:17:34 +0300

    Add homework for Defining Classes

---
+++ C# Advanced/Homeworks-And-Labs/06.DefiningClasses-Exercise/09.PokemonTrainer/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.PokemonTrainer
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+
+        public string Element { get; set; }
+
+        public int Health { get; set; }
+
+        public Pokemon(string name, string element, int health)
+        {
+            this.Name = name;
+            this.Element = element;
+            this.Health = health;
+        }
+    }
+}
---
+++ C# Advanced/Homeworks-And-Labs/06.DefiningClasses-Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public Car(string make, string model, int horsepower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsepower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"HorsePower: {this.HorsePower}");
+            result.Append($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return result.ToString();
+        }
+    }
+}
