67792dc97035d2ded01f027debe250adac36657a
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-09 23:57:05 +0300

    Add homework for Generics

---
+++ C# Advanced/Homeworks-And-Labs/08.Generics-Lab/01.BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        private Stack<T> elements;
+
+        public int Count { get { return this.elements.Count; } }
+
+        public BoxOfT()
+        {
+            this.elements = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Push(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = this.elements.Peek();
+            this.elements.Pop();
+
+            return lastElement;
+        }
+    }
+}
