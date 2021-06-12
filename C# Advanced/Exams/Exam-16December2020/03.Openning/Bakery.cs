b8dedb9d89937aaa6bf4580da3c73045dbc5f063
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-12 23:18:36 +0300

    Exam preparation

---
+++ C# Advanced/Exams/Exam-16December2020/03.Openning/Bakery.cs
@@ -0,0 +1,86 @@
+﻿namespace BakeryOpenning
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Bakery
+    {
+        readonly Dictionary<string, Employee> employees = new Dictionary<string, Employee>();
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get 
+            { 
+                return employees.Count; 
+            } 
+        }   
+
+        public void Add(Employee employee)
+        {
+            if (employees.Count < Capacity)
+            {
+                employees.Add(employee.Name, employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (employees.ContainsKey(name))
+            {
+                employees.Remove(name);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            Employee oldestEmployee = null;
+
+            foreach (var employee in employees
+                .OrderByDescending(e => e.Value.Age))
+            {
+                oldestEmployee = new Employee(employee.Value.Name, employee.Value.Age, employee.Value.Country);
+                break;
+            }
+
+            return oldestEmployee;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = null;
+
+            if (employees.ContainsKey(name))
+            {
+                employee = employees[name];
+                return employee;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"Employee: {employee.Value.Name}, {employee.Value.Age} ({employee.Value.Country})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exams/Exam-16December2020/03.Openning/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace BakeryOpenning
+{
+    using System;
+
+    public class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, {Age} ({Country})";
+        }
+    }
+}
