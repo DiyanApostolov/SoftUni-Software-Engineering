1accc8d93a108ad8efb74970f7fb1ba500241237
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-06-18 23:25:00 +0300

    Update HomeController.cs

--- C# Web Basics/Exams/CarShop/CarShop/Controllers/HomeController.cs
+++ C# Web Basics/Exams/CarShop/CarShop/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿namespace CarShop.Controllers
 {
     using MyWebServer.Controllers;
+    using MyWebServer.Http;
 
     public class HomeController : Controller
     {
-
+        public HttpResponse Index()
+        {
+            return View();
+        }
     }
 }
