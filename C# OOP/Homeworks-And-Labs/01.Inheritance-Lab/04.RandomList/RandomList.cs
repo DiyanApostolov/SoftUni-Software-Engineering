9d4875f607816c2e198394cc1ae226f8f5379da5
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-07-01 09:14:42 +0300

    Add homework for Inheritance

---
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            var index = this.random.Next(0, this.Count);
+            var element = this[index];
+            this.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
---
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var randomList = new RandomList
+            {
+                "Ivan",
+                "Pesho",
+                "Gosho"
+            };
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
