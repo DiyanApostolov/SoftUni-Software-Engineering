fc050b933d64669cc005cdf0e71379f7571da154
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-07-04 16:13:24 +0300

    Add homework

---
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.StackOfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+
+
+        public bool IsEmpty()
+        { 
+            
+        }
+    }
+}
---
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
