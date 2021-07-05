fc2f68d84026e60117972daa9db6ac5e1a6e4717
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-07-05 19:48:11 +0300

    Add homework CustomStack

--- C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _05.StackOfStrings
+namespace CustomStack
 {
     public class StackOfStrings : Stack<string>
     {
-
-
         public bool IsEmpty()
-        { 
-            
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(params string[] collection)
+        {
+            foreach (var element in collection)
+            {
+                this.Push(element);
+            }
         }
     }
 }
--- C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StartUp.cs
+++ C# OOP/Homeworks-And-Labs/01.Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -6,8 +6,14 @@ namespace CustomStack
     {
         static void Main(string[] args)
         {
-            
-            
+            var stringStack = new StackOfStrings();
+
+            Console.WriteLine(stringStack.IsEmpty()); // true
+
+            stringStack.AddRange("1", "2", "3");
+
+            Console.WriteLine(stringStack.IsEmpty()); // false
+
         }
     }
 }
