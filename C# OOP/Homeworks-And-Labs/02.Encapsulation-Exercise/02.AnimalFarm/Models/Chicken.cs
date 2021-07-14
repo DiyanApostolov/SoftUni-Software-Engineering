dc398f939274e85382f83354faee0fe7f48e85b2
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-07-14 21:03:54 +0300

    Add homework for Encapsulation

---
+++ C# OOP/Homeworks-And-Labs/02.Encapsulation-Exercise/02.AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,92 @@
+﻿namespace AnimalFarm.Models
+{
+    using System;
+
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        protected string name;
+        internal int age;
+
+        internal Chicken(string name, int age)
+        {
+            if (String.IsNullOrEmpty(name) || String.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be empty.");
+            }
+            else
+            {
+                this.name = name;
+            }
+
+            if (age >= MinAge && age <= MaxAge)
+            {
+                this.age = age;
+            }
+            else
+            {
+                throw new ArgumentException("Age should be between 0 and 15.");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            internal set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            protected set
+            {
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return this.CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
