dc94c534275280917185ab1ff63a6fba1438e95e
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-07-19 21:14:05 +0300

    Add lab exercise for Reflection and Attributes

---
+++ C# OOP/Homeworks-And-Labs/06.ReflectionAndAttributes-Lab/01.Stealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Hacker", "username", "password");
+
+            Console.WriteLine(result);
+        }
+    }
+}
