8cce1dfeb95956727ae9d49adbc32c21201e99e2
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-08-02 21:48:59 +0300

    OOP Exam preparation - implement methods in controller class

--- C# OOP/Exams/Exam-10April2021/AquaShop/AquaShop/Core/Controller.cs
+++ C# OOP/Exams/Exam-10April2021/AquaShop/AquaShop/Core/Controller.cs
@@ -2,12 +2,15 @@
 {
     using System;
     using System.Collections.Generic;
-
+    using System.Linq;
+    using System.Text;
     using AquaShop.Core.Contracts;
     using AquaShop.Models.Aquariums;
     using AquaShop.Models.Aquariums.Contracts;
     using AquaShop.Models.Decorations;
     using AquaShop.Models.Decorations.Contracts;
+    using AquaShop.Models.Fish;
+    using AquaShop.Models.Fish.Contracts;
     using AquaShop.Repositories;
     using AquaShop.Utilities.Messages;
 
@@ -68,27 +71,89 @@
 
         public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
         {
-            throw new NotImplementedException();
+            bool successfullyAdded = false;
+
+            IAquarium aquarium = aquariums.First(a => a.Name == aquariumName);
+
+            IFish fish = null;
+
+            if (fishType == "FreshwaterFish")
+            {
+                fish = new FreshwaterFish(fishName, fishSpecies, price);
+            }
+            else if (fishType == "SaltwaterFish")
+            {
+                fish = new SaltwaterFish(fishName, fishSpecies, price);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidFishType);
+            }
+
+            if (fish.GetType().Name == "FreshwaterFish" && aquarium.GetType().Name == "FreshwaterAquarium")
+            {
+                successfullyAdded = true;
+            }
+            else if (fish.GetType().Name == "SaltwaterFish" && aquarium.GetType().Name == "SaltwaterAquarium")
+            {
+                successfullyAdded = true;
+            }
+
+            if (successfullyAdded)
+            {
+                aquarium.AddFish(fish);
+            }
+
+            return successfullyAdded
+                ? string.Format(OutputMessages.EntityAddedToAquarium, fishType, aquariumName)
+                : OutputMessages.UnsuitableWater;
         }
 
         public string CalculateValue(string aquariumName)
         {
-            throw new NotImplementedException();
+            IAquarium aquarium = aquariums.First(a => a.Name == aquariumName);
+
+            decimal value = aquarium.Fish.Sum(f => f.Price) + aquarium.Decorations.Sum(d => d.Price);
+
+            return string.Format(OutputMessages.AquariumValue, aquariumName, value);
         }
 
         public string FeedFish(string aquariumName)
         {
-            throw new NotImplementedException();
+            IAquarium aquarium = aquariums.First(a => a.Name == aquariumName);
+
+            aquarium.Feed();
+
+            return string.Format(OutputMessages.FishFed, aquarium.Fish.Count);
         }
 
         public string InsertDecoration(string aquariumName, string decorationType)
         {
-            throw new NotImplementedException();
+            IAquarium aquarium = aquariums.First(a => a.Name == aquariumName);
+            IDecoration decoration = decorations.FindByType(decorationType);
+
+            if (decoration == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentDecoration, decorationType));
+            }
+            
+            aquarium.AddDecoration(decoration);
+
+            decorations.Remove(decoration);
+            
+            return string.Format(OutputMessages.EntityAddedToAquarium, decorationType, aquariumName);
         }
 
         public string Report()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var aquarium in aquariums)
+            {
+               sb.AppendLine(aquarium.GetInfo());
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
