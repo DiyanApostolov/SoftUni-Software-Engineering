7a22e6457165eca70cd8e9fc2338d92b820e0f31
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-08-19 23:10:13 +0300

    Update

--- C# Web Basics/Exams/BattleCards/BattleCards/Models/Users/LoginUserFormModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BattleCards.Models.Users
-{
-    public class LoginUserFormModel
-    {
-        public string Username { get; init; }
-
-        public string Password { get; init; }
-    }
-}
