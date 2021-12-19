20657a2ce7b9056f5f3b654e5760675e68a026e9
Diyan Apostolov <diyan.apostolov@yahoo.com>
2021-12-20 00:03:44 +0200

    Add homework for Algorithms-Fundamentals

---
+++ Algorithms Fundamentals with C#/CombinatorialProblems-Lab/01.PermutationsWithoutRepetition/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.PermutationsWithoutRepetition
+{
+    class Program
+    {
+        private static string[] elements;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ');
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(idx + 1);
+
+            for (int i = idx + 1; i < elements.Length; i++)
+            {
+                Swap(idx, i);
+                Permute(idx + 1);
+                Swap(idx, i);
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
